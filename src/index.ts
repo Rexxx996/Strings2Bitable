236f139c411661465cc61ddf74603df614c0d04e
rexlee11221 <30062517-rexlee11221@users.noreply.replit.com>
2023-12-13 08:39:11 +0000

    Add export file.

--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,9 @@ function hideOverlay() {
   document.getElementById('loadEndOverlay').style.display = 'none';
 }
 
-function updateReadingProgress(statusText, count) {
-  const progressText = statusText + `${count}`;
-  document.getElementById('loadingStatus').innerText = `${progressText}`;
-}
-
-function updateFinishProgress(statusText, current, total) {
+function updateLoadingProgress(statusText, current, total) {
   const progressText = statusText + `${current}/${total}`;
-  document.getElementById('loadEndStatus').innerText = `${progressText}`;
+  document.getElementById('loadingStatus').innerText = `${progressText}`;
 }
 
 function updateAnalyzeProgress(statusText, current, total) {
@@ -34,7 +29,7 @@ function showConfirmation(statusText, current, total) {
   document.getElementById('loadingOverlay').style.display = 'none';
   document.getElementById('loadEndOverlay').style.display = 'flex';
 
-  // document.getElementById('btnConfirm').style.display = 'block';  // 显示确认按钮
+  document.getElementById('btnConfirm').focus();
 }
 
 $('#btnConfirm').on('click', async function() {
@@ -134,21 +129,21 @@ $(async function() {
       langField = await fileTable.getFieldByName<ITextFieldConfig>(inputLang.value);
       console.log('Get language field :', langField);
 
-      currProgress = 0;
-      updateReadingProgress('正在加载数据表：', currProgress);
-
       // 创建一个Map对象(key, value, record id)来存储Record
       const recordMap = new Map<string, string>();
       const recordList = await fileTable.getRecordList();
+      console.log('Get record list:', recordList, recordList.recordIdList.length);
 
-      console.log('Get record list:', recordList, recordList.length);
+      currProgress = 0;
+      totalProgress = recordList.recordIdList.length;
+      updateLoadingProgress('正在加载数据表：', currProgress, totalProgress);
 
       for (const record of recordList) {
         const keyCell = await record.getCellByField(keyField);
         const keyVal = await keyCell.getValue();
 
         currProgress++;
-        updateReadingProgress('正在加载数据表：', currProgress);
+        updateLoadingProgress('正在加载数据表：', currProgress, totalProgress);
 
         // const langCell = await record.getCellByField(langField);
         // const langVal = await langCell.getValue();
@@ -178,7 +173,7 @@ $(async function() {
 
         currProgress = 0;
         totalProgress = keyValuePairs.length;
-        updateAnalyzeProgress('正在解析文件至数据表：', currProgress, totalProgress);
+        updateLoadingProgress('正在解析文件至数据表：', currProgress, totalProgress);
 
         if (keyValuePairs) {
           for (const pair of keyValuePairs) {
@@ -188,7 +183,7 @@ $(async function() {
               const value = matches[2];
 
               currProgress++;
-              updateAnalyzeProgress('正在解析文件至数据表：', currProgress, totalProgress);
+              updateLoadingProgress('正在解析文件至数据表：', currProgress, totalProgress);
 
               if (recordMap.has(key)) {
                 console.log('setCellValue:', langField.id, recordMap.get(key), value);
@@ -223,6 +218,8 @@ $(async function() {
 
         // 加载完成
         showConfirmation('解析文件已完成：', currProgress, totalProgress);
+
+        updateTableList();
       };
 
       reader.readAsText(file);
@@ -230,13 +227,175 @@ $(async function() {
     }
   });
 
+  bitable.base.onTableAdd((event) => {
+    console.log('table added')
+    updateTableList();
+  })
 
-  const [tableList, selection] = await Promise.all([bitable.base.getTableMetaList(), bitable.base.getSelection()]);
-  const optionsHtml = tableList.map(table => {
-    return `<option value="${table.id}">${table.name}</option>`;
-  }).join('');
+  bitable.base.onTableDelete((event) => {
+    console.log('table deleted')
+    updateTableList();
+  })
 
-  $('#tableSelect').append(optionsHtml).val(selection.tableId!);
+  const btnExportFile = document.getElementById('btnExportFile');
+
+  async function updateFieldList(tableId: string) {
+    const currTable = await bitable.base.getTableById(tableId!);
+
+    const fieldList = await currTable.getFieldMetaList();
+
+    // console.log('选中的值为:', currTable);
+
+    // console.log('字段列表为:', fieldList);
+
+    $('#fieldLangSelect').empty();
+
+    const fieldOptionsHtml = fieldList.map(field => {
+      if (field.name !== KEY_FIELD_NAME)
+      {
+        // console.log('字段:', field.id, field.name);
+        return `<option value="${field.id}">${field.name}</option>`;
+      }
+    }).join('');
+
+    
+    // $('#fieldLangSelect').append(fieldOptionsHtml).val(fieldList[0].id!);
+    $('#fieldLangSelect').append(fieldOptionsHtml);
+
+
+    const tableSelect = document.getElementById('tableSelect');
+    const fieldLangSelect = document.getElementById('fieldLangSelect');
+
+    fieldLangSelect.selectedIndex = 0;
+    
+    // console.log('selectedIndex:', tableSelect.selectedIndex, fieldLangSelect.selectedIndex);
+    
+    if (tableSelect.length  !== 0 && fieldLangSelect.length !== 0) {
+      btnExportFile.removeAttribute('disabled');
+    } else {
+      btnExportFile.setAttribute('disabled', true);
+    }
+  }
+
+  async function updateTableList() {
+    const [tableList, tableSelection] = await Promise.all([bitable.base.getTableMetaList(), bitable.base.getSelection()]);
+    const tableOptionsHtml = tableList.map(table => {
+      return `<option value="${table.id}">${table.name}</option>`;
+    }).join('');
+
+    $('#tableSelect').empty();
+    
+    $('#tableSelect').append(tableOptionsHtml).val(tableSelection.tableId!);
+
+    updateFieldList(tableSelection.tableId);
+
+    $('#tableSelect').on('change', async function() {
+      const tableId = $('#tableSelect').val();
+
+      console.log('tableId:', tableId);
+      updateFieldList(tableId);
+    });
+  }
+
+  updateTableList();
+
+  // 导出文件
+  $('#btnExportFile').on('click', async function() {
+
+    let exportTable = null;
+    let exportTableName = null;
+    let exportFieldName = null;
+    let result = null;
+    let langField = null;
+    let keyField = null;
+    let currProgress = 0;
+    let totalProgress = 0;
+    
+    // 显示加载图标
+    showLoadingOverlay();
+
+    const tableId = $('#tableSelect').val();
+
+    exportTableName = tableSelect.options[tableSelect.selectedIndex].text;
+    exportFieldName = fieldLangSelect.options[fieldLangSelect.selectedIndex].text;
+
+    console.log('tableSelect:', exportTableName, tableId);
+    
+    //获取数据表
+    exportTable = await bitable.base.getTableById(tableId);
+
+    console.log('Get table:', exportTable);
+
+    keyField = await exportTable.getFieldByName<ITextFieldConfig>(KEY_FIELD_NAME);
+    console.log('Get key field :', keyField);
+
+    langField = await exportTable.getFieldByName<ITextFieldConfig>(exportFieldName);
+    console.log('Get language field :', langField);
+
+    // 创建一个Map对象(key, value, record id)来存储Record
+    const recordMap = new Map<string, string>();
+    const recordList = await exportTable.getRecordList();
+
+    console.log('Get record list:', recordList, recordList.recordIdList.length);
+
+    currProgress = 0;
+    totalProgress = recordList.recordIdList.length;
+    updateLoadingProgress('正在加载数据表：', currProgress, totalProgress);
+
+    for (const record of recordList) {
+      const keyCell = await record.getCellByField(keyField);
+      const keyVal = await keyCell.getValue();
+
+      const langCell = await record.getCellByField(langField);
+      const langVal = await langCell.getValue();
+      
+      currProgress++;
+      updateLoadingProgress('正在加载数据表：', currProgress, totalProgress);
+
+      // console.log("key lang:", keyVal, langVal);
+
+      if (keyVal) {
+        recordMap.set(keyVal[0].text, langVal[0].text);
+
+        console.log('get record:', keyVal[0].text, langVal[0].text);
+      }
+    }
+
+    // 将键值对转换为 "key" = "value" 格式的字符串
+    const formattedData: string = Array.from(recordMap.entries()).map(([key, value]) => `"${key}" = "${value}"`).join('\n');
+
+    console.log(formattedData);
+    
+    // 创建Blob对象
+    const file: Blob = new Blob([formattedData], {type: ".strings"});
+
+    console.log("file:", file);
+
+    // 创建URL
+    const fileURL: string = URL.createObjectURL(file);
+
+    console.log("fileURL:", fileURL);
+
+    const a = document.getElementById('downloadFile');
+
+    a.href = fileURL;
+    a.download = exportTableName;
+    a.innerText = "下载文件：" + exportTableName + " : " + exportFieldName;
+
+    // 设置点击事件处理器
+    // a.addEventListener('click', () => {
+    //     // 设置定时器以清理资源
+    //     setTimeout(() => {
+    //       console.log("Clean resource:", fileURL);
+    //       window.URL.revokeObjectURL(fileURL);
+    //     }, 10000); // 例如，10秒后清理
+    // });
+    
+    // 加载完成
+    showConfirmation('解析文件已完成：', currProgress, totalProgress);
+  });
+
+  
   $('#addRecord').on('click', async function() {
     const tableId = $('#tableSelect').val();
     if (tableId) {
@@ -246,6 +405,8 @@ $(async function() {
       });
     }
   });
+
+
 });
 
 
